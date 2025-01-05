a6d1cb40e5733ece84d228587ea08cc737350df0
Jacob Mobin <jacobamobin@gmail.com>
2025-01-05 17:56:27 -0500

    Initial Commit

---
+++ AppleInteligenceWatchOS/AppleInteligenceWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleInteligenceWatchOSApp.swift
+//  AppleInteligenceWatchOS
+//
+//  Created by Jacob Mobin on 2025-01-05.
+//
+
+import SwiftUI
+
+@main
+struct AppleInteligenceWatchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
